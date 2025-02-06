782aafdb0878e6e15b47b382bd2b76af5a923f6b
datadiego <juandiegomariscal@gmail.com>
2025-02-06 11:14:39 +0100

    update

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const app = express();
 
+const APIKEY = "1234"
+const mensajes = [];
+
 app.get('/', (req, res) => {
     res.send('Hello World');
+});
+
+app.post("/messages", (req, res) => {
+    const { message } = req.query;
+    mensajes.push(message);
+    res.send("Message created");
+});
+
+app.get("/messages", (req, res) => {
+    res.json(mensajes);
 });
 
 app.listen(3000, () => {
